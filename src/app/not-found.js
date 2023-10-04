ae030b9add04b30349305dc15ebb17024e7775fc
GolamMuid <golammuid@gmail.com>
2023-10-04 16:56:29 +0600

    added data fetching

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+	return (
+		<div className="flex items-center justify-center h-screen font-bold text-5xl">
+			<div>404 Not Found</div>
+		</div>
+	);
+};
+
+export default NotFoundPage;
---
+++ src/app/product/[id]/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+	return (
+		<div className="flex items-center justify-center h-screen font-bold text-5xl">
+			<div>Loading </div>
+		</div>
+	);
+};
+
+export default loading;
